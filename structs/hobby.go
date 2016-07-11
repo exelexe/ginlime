718f5170c9def16bc4744a5536175ed688e37c34
exelexe <deckn.m@gmail.com>
2016-07-11 11:17:12 +0000

    Fix code format

--- structs/hobby.go
+++ structs/hobby.go
@@ -6,7 +6,7 @@ import (
 
 type Hobby struct {
 	Id        int64     `db:"id"`
-  PersonId  int64     `db:"person_id"`
+	PersonId  int64     `db:"person_id"`
 	Name      string    `db:"name"`
 	CreatedAt time.Time `db:"created_at"`
 	UpdatedAt time.Time `db:"updated_at"`
